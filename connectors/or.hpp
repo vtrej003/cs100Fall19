1a8635c648c639bb07fc177cd49883c9b04f7ce7
Andres Estrada <aestr074@student.ucr.edu>
2019-11-11 17:09:22 -0800

    Created add, or, and seperator connnectors as well as the base class from which they inherit.

---
+++ connectors/or.hpp
@@ -0,0 +1,12 @@
+#ifndef __OR_HPP__
+#define __OR_HPP__
+
+class Or : public Base{
+
+public:
+    virtual void execute(std::string){
+     //execute whatever is passed in here.
+	}
+}
+#endif //__OR_HPP__
+
