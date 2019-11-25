1d2c938aefabcb7dcaf22c2fbd3a38d1d504abe2
Andres Estrada <aestr074@student.ucr.edu>
2019-11-25 12:52:15 -0800

    Updated semicolon, and, or connector subclasses.

--- src/parser.cpp
+++ src/parser.cpp
@@ -7,13 +7,14 @@ void Parser::parse(std::vector<std::string> &result ){
 	std::vector<std::string> listOfConnectors({ "&&", "||", ";" });
 	//step one: seperate from commands from connector
 	std::string leftCom, rightCom;
-        std::string  connector;// 1.commands and connector
+        std::string connector;// 1.commands and connector
 	std::string leftExec, rightExec;//2.execs
 	std::string leftArg, rightArg;//3 args
 								  //substring left command
 	std::size_t startOfCon;
 	std::string connectorUsed;
 	bool connectorFound;
+	
 	for (int i = 0; i < cmd.size(); i++) {
 		if (((cmd.at(i) == '&') && (cmd.at(i + 1) == '&')) || ((cmd.at(i) == '|') && (cmd.at(i + 1) == '|')) || (cmd.at(i) == ';' )){
 			startOfCon = cmd.find(cmd.at(i));
@@ -32,23 +33,15 @@ void Parser::parse(std::vector<std::string> &result ){
 	std::size_t lExecPos = leftCom.find(" ");
 	leftExec = leftCom.substr(0, lExecPos);
 	leftArg = leftCom.substr(lExecPos + 1);
+	std::cout<<"This is what is loaded into leftArg:"<<leftArg<<"."<<std::endl;
 
 	//parse right exec alg
 	std::size_t rExecEndPos = rightCom.find(" ");
 	rightExec = rightCom.substr(0, rExecEndPos);
-	rightArg = rightCom.substr(rExecEndPos);
+	rightArg = rightCom.substr(rExecEndPos+1);
 	Command* leftExqt = new Executable(leftExec, leftArg);
 	Command* rightExqt = new Executable(rightExec, rightArg);
-/*
-	if ( connector == "&&"){
-		connector = "0";
-	}
-	else if ( connector == "||"){
-		connector = "1";
-	}	
-	else if ( connector == ";"){
-		connector= "2";
-	}*/
+
 	result.push_back(leftExec); 
 	result.push_back(leftArg); 
 	result.push_back(connector);
--- src/semicolon.cpp
+++ src/semicolon.cpp
@@ -6,10 +6,24 @@ Semicolon::Semicolon(Command* lCom, Command* rCom){
 }
 
  void Semicolon::execute(){
-	leftCommand->execute();
-	rightCommand->execute();
-}
-
- std::string Semicolon::print(){
+    pid_t childPid = fork();
+    int status = -1;
+    if(childPid < 0){//Child not created{
+            perror("fork");
+            exit(EXIT_FAILURE);
+    }
+    else if(childPid !=  0)// this what the parent does
+    {
+        waitpid(childPid, &status, 0);
+        std::cout<<"Executing right\n";
+        rightCommand->execute();        
+    }
+    else //this is what child does
+    {
+        std::cout<<"Executing left\n";
+        leftCommand->execute();
+    }
+}	
+std::string Semicolon::print(){
 
 }
