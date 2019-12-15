0ae70f9c1188ffeaf0d7f49431eaf01e47645e78
Andres Estrada <aestr074@student.ucr.edu>
2019-12-14 23:28:05 -0800

    Fixed Parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -68,31 +68,25 @@ Command* Parser::parse(std::string strToParse){
 /*------------------------------------------------------------------------------*/
 	    else if(cmd.at(i) == '(')
 	    {
-			//std::cout<<"Erasing left parenthesis...\n";
-		//	std::string fileNamed.erase(cmd.begin() + i);
-	     	//std::cout<<"This is what is left of cmd: '"<<cmd<<"'\n";
-			std::size_t rightParPos = cmd.find(')');
-			if (rightParPos != std::string::npos){
-				parenFound = true;
-				std::string pSubstr = cmd.substr(i+1, rightParPos-1);
-				std::size_t lastOfSubstr = cmd.find(pSubstr.back());
-			//	std::cout<<"PARENTHESIS HAS BEEN FOUND.\n";
-			//	std::cout<<"SENDING '"<<pSubstr<< "' TO SUBPARSER...\n";
-				leftCMD = parse(pSubstr);
-			//	std::cout<<"Erasing right parenthesis...\n";
-				cmd.erase(0, pSubstr.length());
-				std::cout<<"This is what it left of command after cutting out parenthesis: '"<< cmd <<"'\n";
+		std::size_t rightParPos = cmd.find(')');
+		if (rightParPos != std::string::npos){
+			parenFound = true;
+			std::string pSubstr = cmd.substr(i+1, rightParPos-1);
+			std::size_t lastOfSubstr = cmd.find(pSubstr.back());
+			//std::cout<<"PARENTHESIS HAS BEEN FOUND.\n";
+			//std::cout<<"SENDING '"<<pSubstr<< "' TO SUBPARSER...\n";
+			leftCMD = parse(pSubstr);
+			//std::cout<<"Erasing right parenthesis...\n";
+			cmd.erase(i, pSubstr.length());
+			std::cout<<"This is what it left of command after cutting out parenthesis: '"<< cmd <<"'\n";
 			//	std::cout<<"This is what is left of cmd: '"<<cmd<<"'\nSubstringing...\n";
 				
-			}
+		}
 	   }
   }
 	std::cout<<"Connector found is '" << connector << "'\n";
-	  if (parenFound == false)
-	  {
-        	
+	  if (parenFound == false){	
 		std::size_t conPos = cmd.find(connector);//if a connector exists, find its pos
-
 		leftStrCMD = cmd.substr(0, conPos-1);
 		if (redirectorFound == true)
        		{
@@ -103,46 +97,41 @@ Command* Parser::parse(std::string strToParse){
 		    std::cout << "exiting redirectorFOund\n:";    
 	        }
 		else
-		{
-                    
-		    std::cout << " did not enter redirector\n" << LOCATION << __LINE__ << std::endl;
+		{         
 		    std::size_t lExecPos = leftStrCMD.find(' ');
 	  	    std::size_t lArgPos = leftStrCMD.find_last_of(' ');
-		    if (leftStrCMD.back() != ' ')
-                    {
+		    if (leftStrCMD.back() != ' '){
 			leftStrCMD += ' ';
 			lArgPos = leftStrCMD.find_last_of(' ');
-
 		    }   
-		    std::cout << "This is the leftStrCMD '" <<leftStrCMD <<"'\n";		
+		    std::cout << "This is the leftStrCMD '" <<leftStrCMD <<"'\n";
 		    arg = leftStrCMD.substr(lExecPos + 1, lArgPos);
 		    exec = cmd.substr(0, lExecPos);
 		    std::cout<<"Connector Found at pos: " << conPos<<std::endl;
 		    std::cout<<"Exec Found at pos: " << lExecPos<<std::endl;
 		    std::cout<<"Command: '" << cmd << "'\n"; 
-		    if ( arg == "" || arg == exec)
-		    {
+		    if ( arg == "" || arg == exec) {
 			std::cout<<"Loading NULL as arg\n";
 			arg = "NULL";
 		    }   
-		    std::cout<<"This is exec:" << exec<<":\n";
-		    std::cout<<"This is arg:" << arg<<":\n";
-		    leftCMD = (instantiate(exec, arg));
+		    std::cout<<"This is exec:'" << exec<<"'\n";
+		    //std::cout<<"This is arg:" << arg<<":\n";
+		    if (arg.back() == ' '){
+                        arg.pop_back();
+                    }
+                    std::cout<<"This is arg:'" << arg<<"'\n";
+		    leftCMD = (instantiate(exec, arg));	    
 		}		
-        	if (connectorFound == true)
-		{
-			std::cout << "entering connector found";
+        	if (connectorFound == true){
+		    std::cout << "entering connector found";
 		    std::size_t startCon = cmd.find(connector);
 		    rightCom = cmd.substr(startCon + connectorSize);
-		    rightCMD = parse(rightCom);
-		
+		    rightCMD = parse(rightCom);	
 		    connectedCMD = (instantiate(leftCMD, rightCMD, connector, parenFound));
 		    return connectedCMD;		
 	        }
-		//single command
-		return leftCMD;
-
-}
+	}
+                return leftCMD;
 }
 Command* Parser::instantiate(std::string exec, std::string args){
         if (exec  == "test") {
