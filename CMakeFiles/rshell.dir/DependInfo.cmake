b9cacbb9462067bc0a8bd6f15576dbcc8610060f
Vincent Trejo <vtrej003@ucr.edu>
2020-01-27 22:56:42 -0800

    back up folder of cs100 assignment

---
+++ CMakeFiles/rshell.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/and.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/and.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/command.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/command.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/executable.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/executable.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/inputRedirect.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/inputRedirect.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/ioRedirect.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/ioRedirect.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/or.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/or.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/outputAppendRedirect.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/outputAppendRedirect.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/outputRedirect.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/outputRedirect.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/pipe.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/pipe.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/query.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/query.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/rshell.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/rshell.cpp.o"
+  "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/src/semicolon.cpp" "/home/csmajs/vtrej003/assignment-andres_vincent_cs_100_assignment/CMakeFiles/rshell.dir/src/semicolon.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/rshell.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rshell.dir/src/query.cpp.o"
+  "CMakeFiles/rshell.dir/src/ioRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/inputRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/outputRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/outputAppendRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/pipe.cpp.o"
+  "CMakeFiles/rshell.dir/src/and.cpp.o"
+  "CMakeFiles/rshell.dir/src/or.cpp.o"
+  "CMakeFiles/rshell.dir/src/semicolon.cpp.o"
+  "CMakeFiles/rshell.dir/src/executable.cpp.o"
+  "CMakeFiles/rshell.dir/src/command.cpp.o"
+  "CMakeFiles/rshell.dir/src/rshell.cpp.o"
+  "rshell.pdb"
+  "rshell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
